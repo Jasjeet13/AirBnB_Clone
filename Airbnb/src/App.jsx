afd8bbeda8ad2083d5052bab23f1e6d1b6fde30a
Jasjeet <jasjeet13.saini@gmail.com>
2024-04-24 14:05:22 +0530

    commit

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -1,35 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
+import BeachCard from './Components/BeachCard'
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-
-    fetch('http://localhost:3000/products')
-      .then(response => response.json())
-      .then(data => {
-
-        setProducts(data);
-      })
-      .catch(error => {
-        console.error('Error fetching products:', error);
-      });
-  }, []);
-
   return (
     <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>Price: ${product.price}</p>
-          </li>
-        ))}
-      </ul>
+      <BeachCard />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
