c862bb42293148cc00092c005124afd7526b9e33
Jasjeet <jasjeet13.saini@gmail.com>
2024-04-25 00:02:42 +0530

    commit-j-3

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import DisplayCard from './Components/DisplayCard'
 import Beach from './Components/Beach'
+import Treehouses from './Components/Treehouses'
+import Amazing_Views from './Components/Amazing_Views'
+import Surfing from './Components/Surfing'
+import Castles from './Components/Castles'
+
 
 function App() {
   return (
     <div>
       {/* <DisplayCard /> */}
 
-      <Beach />
+      {/* <Beach /> */}
+
+      {/* <Treehouses /> */}
+
+      {/* <Amazing_Views /> */}
+
+      {/* <Surfing /> */}
+
+      <Castles />
+
+      
+      
     </div>
   )
 }
---
+++ Airbnb/src/Components/Amazing_Views.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import DisplayCard from './DisplayCard';
+
+function Beach() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/AmazingViews')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      <DisplayCard data={data}/>
+    </div>
+  );
+}
+
+export default Beach;
