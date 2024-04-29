0db3723bb7a1f09685ff75c05ccea9a300bb59bc
Jasjeet <jasjeet13.saini@gmail.com>
2024-04-29 19:28:43 +0530

    commit-j-9

--- Airbnb/src/Components/Section2.jsx
+++ Airbnb/src/Components/Section2.jsx
@@ -10,7 +10,7 @@ export default function Section2() {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-building-check"
+              className="bi bi-building-check"
               viewBox="0 0 16 16"
             >
               <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514" />
@@ -24,7 +24,7 @@ export default function Section2() {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-geo-alt"
+              className="bi bi-geo-alt"
               viewBox="0 0 16 16"
             >
               <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
@@ -37,7 +37,7 @@ export default function Section2() {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-file-medical-fill"
+              className="bi bi-file-medical-fill"
               viewBox="0 0 16 16"
             >
               <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M8.5 4.5v.634l.549-.317a.5.5 0 1 1 .5.866L9 6l.549.317a.5.5 0 1 1-.5.866L8.5 6.866V7.5a.5.5 0 0 1-1 0v-.634l-.549.317a.5.5 0 1 1-.5-.866L7 6l-.549-.317a.5.5 0 0 1 .5-.866l.549.317V4.5a.5.5 0 1 1 1 0M5.5 9h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1m0 2h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1" />
