148c57d68a12f67f9e0da6c9187dbd3a5fa0a358
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-04-29 15:36:00 +0530

    commitk9

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import DateSelector from "./Components/DateSelector";
 import SearchBox from "./Components/SearchBox";
+import AboutPlace from "./Components/AboutPlace";
+import Reserve from "./Components/Reserve";
 function App() {
   return (
     <div>
       <DateSelector />
       <SearchBox />
+      <Reserve />
     </div>
   );
 }
