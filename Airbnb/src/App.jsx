5cadf3611d17c3ae298083f1920723dde380188a
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-05-02 22:48:31 +0530

    commitk18

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { useState } from 'react'
-import DisplayCard from './Components/DisplayCard'
-import Beach from './Components/Beach'
-import Treehouses from './Components/Treehouses'
-import Amazing_Views from './Components/Amazing_Views'
-import Surfing from './Components/Surfing'
-import Castles from './Components/Castles'
-import Categories from './Components/Categories'
-import ImageLayout from './Components/ImageLayout'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import ShowMore from './Components/ShowMore'
-import ShowMore2 from './Components/ShowMore2'
-import AboutPlace from './Components/AboutPlace'
-import DateSelector from './Components/DateSelector'
-import Navbar from './Components/Navbar'
-import SearchBox from './Components/SearchBox'
-import { Container, Grid } from '@mui/material'
-import Footer from './Components/Footer'
+import React from "react";
+import { useState } from "react";
+import DisplayCard from "./Components/DisplayCard";
+import Beach from "./Components/Beach";
+import Treehouses from "./Components/Treehouses";
+import Amazing_Views from "./Components/Amazing_Views";
+import Surfing from "./Components/Surfing";
+import Castles from "./Components/Castles";
+import Categories from "./Components/Categories";
+import ImageLayout from "./Components/ImageLayout";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ShowMore from "./Components/ShowMore";
+import ShowMore2 from "./Components/ShowMore2";
+import AboutPlace from "./Components/AboutPlace";
+import DateSelector from "./Components/DateSelector";
+import Navbar from "./Components/Navbar";
+import SearchBox from "./Components/SearchBox";
+import { Container, Grid } from "@mui/material";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <Router>
       <div>
-        <Grid container sx={{borderBottom:'solid 1px rgba(0, 0, 0, 0.1)'}}>
-          <Grid item xs={12} >
+        <Grid container sx={{ borderBottom: "solid 1px rgba(0, 0, 0, 0.1)" }}>
+          <Grid item xs={12}>
             <Navbar />
           </Grid>
 
@@ -31,25 +31,29 @@ function App() {
             <SearchBox />
           </Grid>
 
-          
-          <Grid item xs={12} sx={{marginBottom:'1%',borderTop:'solid 1px rgba(0, 0, 0, 0.1)'}}>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              marginBottom: "1%",
+              borderTop: "solid 1px rgba(0, 0, 0, 0.1)",
+            }}
+          >
             <Categories />
           </Grid>
-
         </Grid>
       </div>
       <Routes>
-        <Route path='/' element={<Surfing />}/>
-        <Route path='/Surfing' element={<Surfing />}/>
-        <Route path='/Surfing' element={<Surfing />}/>
-        <Route path='/Castles' element={<Castles />}/>
-        <Route path='/AmazingViews' element={<Amazing_Views />}/>
-        <Route path='/Treehouses' element={<Treehouses />}/>
-        <Route path='/Beaches' element={<Beach />}/>
-        <Route path='/:category/:placeId' element={<AboutPlace/>} />
+        <Route path="/" element={<Surfing />} />
+        <Route path="/Surfing" element={<Surfing />} />
+        <Route path="/Surfing" element={<Surfing />} />
+        <Route path="/Castles" element={<Castles />} />
+        <Route path="/AmazingViews" element={<Amazing_Views />} />
+        <Route path="/Treehouses" element={<Treehouses />} />
+        <Route path="/Beaches" element={<Beach />} />
+        <Route path="/:category/:placeId" element={<AboutPlace />} />
       </Routes>
     </Router>
-
   );
 }
 
--- Airbnb/src/Components/Navbar.jsx
+++ Airbnb/src/Components/Navbar.jsx
@@ -1,44 +1,46 @@
-import React, { useState } from 'react';
-import './Navbar.css';
+import React, { useState } from "react";
+import "./Navbar.css";
 import logo from "./assets/logo.jpg";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
+// import SearchBox from "./SearchBox";
 
 function Navbar() {
-    const [showDropdown, setShowDropdown] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
-    const toggleDropdown = () => {
-        setShowDropdown(!showDropdown);
-    };
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <img src={logo} alt="Logo" style={{ height: '100%' }} />
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" style={{ height: "100%" }} />
+      </div>
+      <div className="navbar-links">
+        <p style={{fontSize:'15px',fontWeight:'600'}}>Stays</p>
+        {/* <SearchBox /> */}
+      </div>
+      <div className="navbar-account">
+        <a href="#menu" onClick={toggleDropdown}>
+          <FontAwesomeIcon icon={faBars} color="grey" fontSize="12px" />
+        </a>
+        <a href="#profile" onClick={toggleDropdown}>
+          <FontAwesomeIcon icon={faUser} color="grey" />
+        </a>
+        {showDropdown && (
+          <div className="dropdown-menu">
+            <div className="login">
+              <p>Login</p>
             </div>
-            <div className="navbar-links">
-                <p style={{fontSize:'15px',fontWeight:'600'}}>Stays</p>
+            <div className="signup">
+              <p>Sign up</p>
             </div>
-            <div className="navbar-account">
-                <a href="#menu" onClick={toggleDropdown}>
-                    <FontAwesomeIcon icon={faBars} color="grey" fontSize="12px"/>
-                </a>
-                <a href="#profile" onClick={toggleDropdown}>
-                    <FontAwesomeIcon icon={faUser} color="grey"/>
-                </a>
-                {showDropdown && (
-                    <div className="dropdown-menu">
-                      <div className='login'>
-                        <p>Login</p>
-                      </div>
-                      <div className='signup'>
-                        <p>Sign up</p>
-                      </div>
-                    </div>
-                )}
-            </div>
-        </nav>
-    )
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
